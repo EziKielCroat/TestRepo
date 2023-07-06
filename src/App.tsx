4e738b73d6881cccb70ce4860b243a657c0a79d2
Duje Zizic <duje@caelor.com>
2023-07-06 16:29:13 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
- 
+import "./App.css";
+
 function App() {
- 
   return (
     <div className="App">
       <h1>Uvod u git</h1>
       <p>Praćenje promjena u repozitoriju</p>
+      <p>AAA</p>
     </div>
-  )
+  );
 }
- 
-export default App
+
+export default App;
